b1c658160f6dcdb18774772efa160d507ba3f749
Ankur Soni <ankurson@amazon.com>
2024-06-13 22:02:51 -0700

    my commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,66 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import { NativeBaseProvider, Box, Button, Stack, Slider } from "native-base";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <NativeBaseProvider>
+      <Box alignItems="center">Hello</Box>
+      <Button onPress={() => console.log("hello world")}>Click Me</Button>
+      <Stack
+        direction={{
+          base: "column",
+          md: "row",
+        }}
+        space={2}
+        alignItems={{
+          base: "center",
+          md: "flex-start",
+        }}
+      >
+        <Button isLoading>Button</Button>
+        <Button isLoading isLoadingText="Submitting">
+          Button
+        </Button>
+        <Button isLoading spinnerPlacement="end" isLoadingText="Submitting">
+          Button
+        </Button>
+        <Button
+          isLoading
+          _loading={{
+            bg: "amber.400:alpha.70",
+            _text: {
+              color: "coolGray.700",
+            },
+          }}
+          _spinner={{
+            color: "white",
+          }}
+          isLoadingText="Submitting"
+        >
+          Button
+        </Button>
+        <Button isLoading isLoadingText="Submitting" variant="outline">
+          Button
+        </Button>
+        <Box alignItems="center" w="100%">
+          <Slider
+            w="3/4"
+            maxW="300"
+            defaultValue={70}
+            minValue={0}
+            maxValue={100}
+            accessibilityLabel="hello world"
+            step={10}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <Slider.Track>
+              <Slider.FilledTrack />
+            </Slider.Track>
+            <Slider.Thumb />
+          </Slider>
+        </Box>
+      </Stack>
+    </NativeBaseProvider>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,36 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const packages = [
+    'react-native',
+    '@amzn/payments-components-react-native',
+    "react-native-web",
+    "react-native-svg",
+    "native-base",
+  ]
+  
+  const nextConfig = {
+    transpilePackages: packages,
+    webpack: (config) =>
+      Object.assign(config, {
+        resolve: Object.assign(config.resolve, {
+          alias: {
+            ...(config.resolve.alias || {}),
+            // Transform all direct `react-native` imports to `react-native-web`
+            "react-native$": "react-native-web",
+          },
+          extensions: [
+            ".web.ts",
+            ".web.tsx",
+            ".ts",
+            ".tsx",
+            ".web.js",
+            ".web.jsx",
+            ".js",
+            ".jsx",
+            ...config.resolve.extensions,
+          ],
+        }),
+      }),
+  };
+  
+  export default nextConfig;
+  
